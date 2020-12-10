b7593b0612df29ded6fb9eb5136f74b5394aee9d
Seelachsfilet <68815284+Seelachsfilet@users.noreply.github.com>
2020-12-10 13:14:26 +0100

    Create server-monitor.py

---
+++ server-monitor.py
@@ -0,0 +1,18 @@
+### AUTHOR: https://github.com/Seelachsfilet
+### AUTHOR-MAIL: seelachsfilet.all@web.de
+### VERSION: 1.0
+### LANGUAGE: [DE][EN]
+### If you record a video with this file, I want me to be involved, that means that you have to add my name!
+### Republication is prohibited!
+###
+variable = input("Bitte gebe das Script an: \n")
+
+if(variable == "http"):
+    print("Du hast die variable")
+    print(variable)
+
+if(variable == "ssh"):
+    print("Du hast die variable")
+    print(variable)
+
+
