5d841ac9a9b31e3508baa4ed67d01bbad7babda6
Seelachsfilet <68815284+Seelachsfilet@users.noreply.github.com>
2020-12-10 19:52:12 +0100

    Update server-monitor.py

--- server-monitor.py
+++ server-monitor.py
@@ -5,14 +5,23 @@
 ### If you record a video with this file, I want me to be involved, that means that you have to add my name!
 ### Republication is prohibited!
 ###
+print("Es wird bis jetzt nur auf 3 Protokollen einen server-monitor geben: ftp, ssh, http, https")
 variable = input("Bitte gebe das Script an: \n")
 
+
+
 if(variable == "http"):
-    print("Du hast die variable")
-    print(variable)
+    print(f"Du hast das Protokoll: |{variable}| gewählt")
+    print(f"Es wird nun probiert ob das Endgerät Pings animmt!")
 
-if(variable == "ssh"):
-    print("Du hast die variable")
-    print(variable)
 
+if(variable == "ssh" or "SSH"):
+    print(f"Du hast das Protokoll: |{variable}| gewählt")
+    print(f"Es wird nun probiert ob das Endgerät Pings animmt!")
+    os = input("Bitte gebe deinen Betriebssystemnamen ein: ")
+    
+
+if(variable == "ftp"):
+    print(f"Du hast das Protokoll: |{variable}| gewählt")
+    print(f"Es wird nun probiert ob das Endgerät Pings animmt!")
 
