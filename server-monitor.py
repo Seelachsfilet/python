bcd93cad79c27ab92a5c867ddde94e90d4e7553b
Seelachsfilet <68815284+Seelachsfilet@users.noreply.github.com>
2020-12-11 21:22:26 +0100

    Update server-monitor.py

--- server-monitor.py
+++ server-monitor.py
@@ -5,23 +5,39 @@
 ### If you record a video with this file, I want me to be involved, that means that you have to add my name!
 ### Republication is prohibited!
 ###
+import os
+
+
 print("Es wird bis jetzt nur auf 3 Protokollen einen server-monitor geben: ftp, ssh, http, https")
-variable = input("Bitte gebe das Script an: \n")
+variable_de = input("Bitte gebe das Script an: \n")
+language = input("Bitte Geb deine sprache in DE oder EN an \n")
+ip = input("Geb die IP des clients an den du monitoren willst: \n")
+if os.system("ping -c 1 " + ip) == 0:
+    print ("IP ist erreichbar")
+else:
+    print ("IP ist NICHT erreichbar")
 
 
 
-if(variable == "http"):
-    print(f"Du hast das Protokoll: |{variable}| gewählt")
-    print(f"Es wird nun probiert ob das Endgerät Pings animmt!")
+if(language == "DE" or "de"):
+    print(f"Das programm wurde auf |{language}| gestellt")
 
 
-if(variable == "ssh" or "SSH"):
-    print(f"Du hast das Protokoll: |{variable}| gewählt")
-    print(f"Es wird nun probiert ob das Endgerät Pings animmt!")
-    os = input("Bitte gebe deinen Betriebssystemnamen ein: ")
-    
+    if(variable_de == "http"):
+        print(f"Du hast das Protokoll: |{variable_de}| gewählt")
+        print(f"Es wird nun probiert ob das Endgerät Pings animmt!")
 
-if(variable == "ftp"):
-    print(f"Du hast das Protokoll: |{variable}| gewählt")
-    print(f"Es wird nun probiert ob das Endgerät Pings animmt!")
+
+    if(variable_de == "ssh" or "SSH"):
+        print(f"Du hast das Protokoll: |{variable_de}| gewählt")
+        print(f"Es wird nun probiert ob das Endgerät Pings animmt!")
+        os = input("Bitte gebe deinen Betriebssystemnamen ein: ")
+        if(os == "windows" or "Windows"):
+            print(f"Dein Betriebssystem ist : {os}")
+            print("Windoiw")
+
+
+    if(variable_de == "ftp"):
+        print(f"Du hast das Protokoll: |{variable_de}| gewählt")
+        print(f"Es wird nun probiert ob das Endgerät Pings animmt!")
 
